a9c23d0ca2a9ec8c456aecb8b91d0bcf47476222
alemoretto <a.moretto86@gmail.com>
2019-06-21 12:39:37 +0200

    revers FATTO

--- src/main/java/it/prova/pokeronline/model/Tavolo.java
+++ src/main/java/it/prova/pokeronline/model/Tavolo.java
@@ -21,7 +21,7 @@ public class Tavolo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
+	private String denominazione;
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "utenteCreatore_id", nullable = false)
 	private Utente creatore;
@@ -49,6 +49,14 @@ public class Tavolo {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getDenominazione() {
+		return denominazione;
+	}
+
+	public void setDenominazione(String denominazione) {
+		this.denominazione = denominazione;
 	}
 
 	public Utente getCreatore() {
